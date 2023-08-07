43db58deedc2b3e7e943b01c252afa5c5e89a32a
okari <nyandika15@gmail.com>
2023-08-08 00:22:13 +0300

    Fix linter

--- src/components/Number.js
+++ src/components/Number.js
@@ -1,20 +1,20 @@
 function Number() {
   return (
     <div className="numbers">
-      <button type="button" className="button">AC</button>
-      <button type="button" className="button">+/-</button>
-      <button type="button" className="button">%</button>
-      <button type="button" className="button">7</button>
-      <button type="button" className="button">8</button>
-      <button type="button" className="button">9</button>
-      <button type="button" className="button">4</button>
-      <button type="button" className="button">5</button>
-      <button type="button" className="button">6</button>
-      <button type="button" className="button">1</button>
-      <button type="button" className="button">2</button>
-      <button type="button" className="button">3</button>
-      <button type="button" className="button">0</button>
-      <button type="button" className="button">.</button>
+      <button type="button" className="number">AC</button>
+      <button type="button" className="number">+/-</button>
+      <button type="button" className="number">%</button>
+      <button type="button" className="number">7</button>
+      <button type="button" className="number">8</button>
+      <button type="button" className="number">9</button>
+      <button type="button" className="number">4</button>
+      <button type="button" className="number">5</button>
+      <button type="button" className="number">6</button>
+      <button type="button" className="number">1</button>
+      <button type="button" className="number">2</button>
+      <button type="button" className="number">3</button>
+      <button type="button" className="number">0</button>
+      <button type="button" className="number">.</button>
     </div>
   );
 }
