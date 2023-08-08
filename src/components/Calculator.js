3d99ac7576098feb19031fe610e08af6ef1a7ac1
okari <nyandika15@gmail.com>
2023-08-08 17:18:33 +0300

    Initial commit

--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -2,6 +2,7 @@ import './Calculator.css';
 import Number from './Number';
 import Operand from './Operands';
 import Screen from './Screen';
+import calculate from '../logic/calculate';
 
 function Calculator() {
   return (
