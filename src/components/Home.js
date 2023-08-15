7b7cf92f728a2bb3101c9fcf150aed4c04041728
Okari Nyandika <nyandika15@icloud.com>
2023-08-15 17:56:00 +0300

    Create Home.js

---
+++ src/components/Home.js
@@ -0,0 +1,28 @@
+function HomePage() {
+  return (
+    <>
+      <h1 className="welcome-text">Welcome to our Page!</h1>
+      <div className="description-div">
+        <p className="description-text">
+          It is important to take care of the pain itself, and it will be followed by
+           the growth of the patient, but at the same time it will happen that there 
+           is a lot of work and pain. As for I will come to the smallest detail, who
+            should practice any kind of work unless it results in some advantage. Do not rush into the house
+          he will criticize in pleasure he wants to be a hair in pain, let him run away from the joy of
+           nothing. Unless they are blinded by lust, they do not come out; they are in fault who abandon their
+            duty, and the soul is softened, that is toil. Him
+          <br />
+          It is important to take care of the pain itself, and it will be followed by
+           the growth of the patient, but at the same time it will happen that there is a lot of work and pain. As
+          for I will come to the least, who does not exercise any kind of labor unless
+           he obtains some benefit from it. Do not rush into the house
+          he will criticize in pleasure he wants to be a hair in pain, let him run away
+           from the joy of nothing. Unless they are blinded by lust, they do not come forth;
+            they are in fault who abandon their duties and soften their hearts, that is, their labors. Him
+        </p>
+      </div>
+    </>
+  );
+}
+
+export default HomePage;
