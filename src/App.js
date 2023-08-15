83f4d46702db0705a9db1df7b039a76cfb3a53e2
okari <nyandika15@gmail.com>
2023-08-15 18:28:32 +0300

    ss

--- src/App.js
+++ src/App.js
@@ -1,26 +1,28 @@
 import './App.css';
+import {
+  Routes, Route, Outlet, Link,
+} from 'react-router-dom';
 import Quote from './components/QuoteApi';
 import Calculator from './components/Calculator';
 import HomePage from './components/Home';
-import { Routes, Route, Outlet, Link } from "react-router-dom";
 
 function App() {
   return (
 
     <>
-    <nav>
-      <h2>Math Magicians</h2>
-      <ul>
-        <li><Link to="/" >Home</Link></li>
-        <li><Link to="/calculator" >Calculator</Link></li>
-        <li><Link to="/quotes" >Quote</Link></li>
-      </ul>
-    </nav>
-    <Routes>
-      <Route path='/' element={<HomePage />} />
-      <Route path='/calculator' element={<Calculator />} />
-      <Route path='/quotes' element={<Quote />} />
-    </Routes>
+      <nav>
+        <h2>Math Magicians</h2>
+        <ul>
+          <li><Link to="/">Home</Link></li>
+          <li><Link to="/calculator">Calculator</Link></li>
+          <li><Link to="/quotes">Quote</Link></li>
+        </ul>
+      </nav>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/calculator" element={<Calculator />} />
+        <Route path="/quotes" element={<Quote />} />
+      </Routes>
       {/* <Calculator />
       <Quote /> */}
     </>
